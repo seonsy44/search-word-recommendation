add16824db9c1e261375290ee610353ae053ded4
seon <seonsy44@gmail.com>
2022-11-10 00:53:25 +0900

    feat: theme provider type 선언

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+import 'styled-components';
+
+declare module 'styled-components' {
+  interface DefaultTheme {
+    primary: '#357AE1';
+    secondary: '#D0E8FD';
+    gray: '#A6AFB7';
+  }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,5 @@
 export const colors = {
   primary: '#357AE1',
   secondary: '#D0E8FD',
+  gray: '#A6AFB7',
 } as const;
